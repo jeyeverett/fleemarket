d2383ca64a91a879f61eef86eab77b1f7ce61b55
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-07-20 13:52:19 -0400

    modify admin.js to reload page when all products deleted

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,25 +1,25 @@
 const deleteProduct = (btn) => {
-    const container = btn.parentNode.parentNode;
-    const csrf = container.querySelector('[name=_csrf').value;
-    const id = container.querySelector('[name=id').value;
-    const imageUrl = container.querySelector('[name=imageUrl').value;
-    const productElement = container.closest('article');
+  const container = btn.closest('article');
+  const parent = container.parentNode;
+  const csrf = container.querySelector('[name=_csrf').value;
+  const id = container.querySelector('[name=id').value;
+  const imageUrl = container.querySelector('[name=imageUrl').value;
 
-    fetch(`/admin/product/${id}?imageUrl=${imageUrl}`, {
-        method: 'DELETE',
-        headers: {
-            'csrf-token': csrf
-        },
-        body: {
-            isAuthenticated: true
-        }
+  fetch(`/admin/product/${id}?imageUrl=${imageUrl}`, {
+    method: 'DELETE',
+    headers: {
+      'csrf-token': csrf,
+    },
+    body: {
+      isAuthenticated: true,
+    },
+  })
+    .then((res) => res.json())
+    .then((data) => {
+      container.remove();
+      if (!parent.childElementCount) {
+        location.reload();
+      }
     })
-    .then(result => {
-        return result.json();
-    })
-    .then(data => {
-        console.log(data);
-        productElement.remove();
-    })
-    .catch(err => console.log(err));
+    .catch((err) => console.log(err));
 };
