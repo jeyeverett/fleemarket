b84853e45a48e85b4e5213fce22993a80f17fa33
Jeysen Freedman <jeysenfreedman@gmail.com>
2021-07-20 13:50:29 -0400

    fix delete file utility funciton

--- utilities/file.js
+++ utilities/file.js
@@ -1,13 +1,11 @@
 const fs = require('fs');
 
-const deleteFile = filePath => {
-    fs.unlink(filePath, err => {
-        if (err) {
-            throw (err);
-        }
-    })
-}
+const deleteFile = (filePath) => {
+  fs.unlink(filePath, (err) => {
+    if (err) console.log(err);
+  });
+};
 
 module.exports = {
-    deleteFile
-}
+  deleteFile,
+};
